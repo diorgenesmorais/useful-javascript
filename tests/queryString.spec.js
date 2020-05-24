dd6420d751833be615bb421e1ed28d5d544669e1
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-05-24 11:46:08 -0300

    convertPayloadToQueryString test passing json

--- tests/queryString.spec.js
+++ tests/queryString.spec.js
@@ -6,4 +6,15 @@ describe('QueryString tests', () => {
         expect(queryString()).to.exist;
         expect(queryString()).to.have.any.keys('convertPayloadToQueryString');
     });
+
+    it('Should get a query string of the json', () => {
+        const payload = {
+            nome: 'Diorgenes Morais',
+            instalacao: '00124578',
+            cidade: 'São Lourenço da Mata',
+            estado: 'PE'
+        }
+        expect(queryString().convertPayloadToQueryString(payload))
+            .to.equals('nome=Diorgenes%20Morais&instalacao=00124578&cidade=S%C3%A3o%20Louren%C3%A7o%20da%20Mata&estado=PE');
+    });
 });
