295f724a89e6cd87083e5aad069684d10ca65ec1
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-05-24 12:26:26 -0300

    create convertQueryStringToArray method and test

--- src/queryString.js
+++ src/queryString.js
@@ -11,8 +11,34 @@ const queryString = function() {
                         .join('&');
     }
 
+    /**
+     * Converter uma queryString para um Array.
+     * 
+     * @param {string} queryString (key=value&key=value&key=value)
+     * @returns {array} onde index vai ser a key.
+     */
+    function convertQueryStringToArray(queryString) {
+        const array = [];
+        queryString.split('')
+            .map(letra => {
+                if (letra === '&') {
+                    return '=';
+                }
+                return letra;
+            })
+            .join('') // retorna tudo em uma string
+            .split('=') // retorna uma lista: ['key', 'value', 'key', 'value']
+            .forEach((value, index, list) => {
+                if (index % 2 === 0) {
+                    array[value] = decodeURIComponent(list[index + 1]);
+                }
+            });
+        return array;
+    }
+
     return {
-        convertPayloadToQueryString
+        convertPayloadToQueryString,
+        convertQueryStringToArray
     }
 }
 
--- tests/queryString.spec.js
+++ tests/queryString.spec.js
@@ -2,6 +2,8 @@ const expect = require('chai').expect;
 const queryString = require('../src/queryString');
 
 describe('QueryString tests', () => {
+    const useful = new queryString();
+
     it('Should exists', () => {
         expect(queryString()).to.exist;
         expect(queryString()).to.have.any.keys('convertPayloadToQueryString');
@@ -17,4 +19,14 @@ describe('QueryString tests', () => {
         expect(queryString().convertPayloadToQueryString(payload))
             .to.equals('nome=Diorgenes%20Morais&instalacao=00124578&cidade=S%C3%A3o%20Louren%C3%A7o%20da%20Mata&estado=PE');
     });
+
+    const query = queryString().convertPayloadToQueryString(payload);
+
+    it('Should get an array of the query string', () => {
+        const array = useful.convertQueryStringToArray(query);
+        expect(array.nome).to.equal('Diorgenes Morais');
+        expect(array.instalacao).to.equal('00124578');
+        expect(array.cidade).to.equal('São Lourenço da Mata');
+        expect(array.estado).to.equal('PE');
+    })
 });
